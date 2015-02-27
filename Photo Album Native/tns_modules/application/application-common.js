1a507ad49668bf68e11f1ddc5156000bca8cf36a
Nikolay Diyanov <nikolay.diyanov@telerik.com>
2015-02-27 12:03:03 +0200

    removing old project

--- Photo Album Native/tns_modules/application/application-common.js
+++
@@ -1,9 +0,0 @@
-require("globals");
-exports.onUncaughtError = undefined;
-exports.onLaunch = undefined;
-exports.onSuspend = undefined;
-exports.onResume = undefined;
-exports.onExit = undefined;
-exports.onLowMemory = undefined;
-exports.android = undefined;
-exports.ios = undefined;
